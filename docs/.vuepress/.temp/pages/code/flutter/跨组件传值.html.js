e34532e69b9435c4bab6fd51895c6b5b5a74c6d7
w2gd <W2GDong@gmail.com>
2022-09-20 15:35:54 +0800

    跨组件传值

---
+++ docs/.vuepress/.temp/pages/code/flutter/跨组件传值.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-c34a56ec\",\"path\":\"/code/flutter/%E8%B7%A8%E7%BB%84%E4%BB%B6%E4%BC%A0%E5%80%BC.html\",\"title\":\"跨组件传值\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"跨组件传值\",\"icon\":\"dart\",\"date\":\"2022-09-20T00:00:00.000Z\",\"category\":[\"flutter\"],\"order\":7,\"summary\":\"传值方法 优点 缺点 :-- :-- :-- 属性传值 使用简单 跨越很多层传递给子组件繁琐而且冗余 InheritedWidget 跨多个父子节点方便 修改数据麻烦，传值方向的单一，从上往下 Notification 向上传递事件方便 监听通知读取麻烦，传值方向的单一，从下往上 EventBus 订阅、发布。全局使用，无需组件间存在父子关系 要订阅和解订阅\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://vuepress-theme-hope-v2-demo.mrhope.site/code/flutter/%E8%B7%A8%E7%BB%84%E4%BB%B6%E4%BC%A0%E5%80%BC.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"WD\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"跨组件传值\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:image\",\"content\":\"https://vuepress-theme-hope-v2-demo.mrhope.site/\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"name\":\"twitter:card\",\"content\":\"summary_large_image\"}],[\"meta\",{\"name\":\"twitter:image:alt\",\"content\":\"跨组件传值\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"2022-09-20T00:00:00.000Z\"}]]},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"InheritedWidget\",\"slug\":\"inheritedwidget\",\"children\":[]},{\"level\":2,\"title\":\"Notification\",\"slug\":\"notification\",\"children\":[]},{\"level\":2,\"title\":\"event_bus\",\"slug\":\"event-bus\",\"children\":[]}],\"readingTime\":{\"minutes\":3.55,\"words\":1065},\"copyright\":\"著作权归所有\\n原文链接：https://vuepress-theme-hope-v2-demo.mrhope.site/code/flutter/%E8%B7%A8%E7%BB%84%E4%BB%B6%E4%BC%A0%E5%80%BC.html\",\"filePathRelative\":\"code/flutter/跨组件传值.md\",\"localizedDate\":\"2022年9月20日\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
