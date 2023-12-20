d21145f183a46eb3b9a95aab539fd0d1a87a75b5
w2gd <W2GDong@gmail.com>
2023-12-20 10:34:24 +0800

    日寄

---
+++ docs/.vuepress/.temp/pages/category/日寄/index.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-2e33fa57\",\"path\":\"/category/%E6%97%A5%E5%AF%84/\",\"title\":\"日寄 分类\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"日寄 分类\",\"blog\":{\"type\":\"category\",\"name\":\"日寄\",\"key\":\"category\"},\"layout\":\"Blog\",\"summary\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://vuepress-theme-hope-v2-demo.mrhope.site/category/%E6%97%A5%E5%AF%84/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"WD\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"日寄 分类\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0,\"words\":0},\"copyright\":\"著作权归所有\\n原文链接：https://vuepress-theme-hope-v2-demo.mrhope.site/category/%E6%97%A5%E5%AF%84/\",\"filePathRelative\":null}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
